12f51298fdfb83706fefc9eec624ed01987827f6
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-09 11:01:08 +0530

    Functions & Vars

---
+++ 03-vars.sh
@@ -0,0 +1,5 @@
+a=10
+echo "a is $a"
+
+log=/tmp/roboshop.log
+cat $log # it will give the /tmp/roboshop.log file output
---
+++ 04-functions.sh
@@ -0,0 +1,9 @@
+greeting() {
+  echo Hello, Good Morning.
+  echo Welcome to Devops Training.
+  echo Good to Have you hear.
+}
+
+# So by using function we can pass multiple command in a single.
+# here greeting will give the output of mentioned three echo command.
+greeting

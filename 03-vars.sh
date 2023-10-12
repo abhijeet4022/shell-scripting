dbc2ec7e6018ee9e5dfbcb6426a1dba5a24f9f86
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-12 10:31:54 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,5 +1,24 @@
+#variable
 a=10
 echo "a is $a"
 
 log=/tmp/roboshop.log
 cat $log # it will give the /tmp/roboshop.log file output
+
+#Special Variables
+# $0 - $N, &*, $#
+
+#Substitution Variables
+## Command Substitute
+DATE=$(date)   #Here we have to pass this bracket ().
+echo Today date is $DATE
+
+
+#Arithmetic Substitution
+ADD=$(( 2+2 ))     #Here we have to pass this two bracket ().
+echo ADD of 2+2 = $ADD
+
+
+
+
+

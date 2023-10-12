e9bc1ed8eab5ef622d5d4305d428c1e1e0cfc257
abhijeet4022 <abhijeet4022@gmail.com>
2023-10-12 10:58:35 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -2,8 +2,8 @@
 a=10
 echo "a is $a"
 
-log=/tmp/roboshop.log
-cat $log # it will give the /tmp/roboshop.log file output
+#log=/tmp/roboshop.log
+#cat $log # it will give the /tmp/roboshop.log file output
 
 #Special Variables
 # $0 - $N, &*, $#
--- 04-functions.sh
+++ 04-functions.sh
@@ -7,3 +7,19 @@ greeting() {
 # So by using function we can pass multiple command in a single.
 # here greeting will give the output of mentioned three echo command.
 greeting
+echo function exit status - $?
+
+#exit: If we want to comeout from script we will pass exit
+#return : if we want to exit from funtion then we will pass return
+
+# We declare var in main program, we can access that in function and vice-versa.
+# Function have its own special variables.
+
+input(){
+  echo First Argument - $1
+  echo Senond Argument - $2
+  echo All Arguments - $*
+  echo No of Arguments - $#
+}
+
+input abc 1234
